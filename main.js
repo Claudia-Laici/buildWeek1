65d3649a5828853b0604a9cae8c74aa0002cc421
Islam95-ellaket <ellaket.islam95@gmail.com>
2026-05-19 20:24:45 +0200

    aggiunto .hidden display none nel css

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 
 let proceedBtn = document.getElementById('proceed-btn');
-let divQuestions = document.getElementById('questions')
-let checked = false
-/* let promiseCheckbox = document.getElementById('promise-check').checked; */
-
-
+let divQuestions = document.getElementById('questions');
 const promiseCheckbox = document.getElementById("promise-check");
+let welcomePage = document.querySelector(".welcome-content")
 
-promiseCheckbox.addEventListener("change", function prosegui() {
+promiseCheckbox.addEventListener("change", function () {
     if (this.checked) {
         proceedBtn.disabled = false;
         proceedBtn.className = "btn-enabled";
@@ -16,4 +13,10 @@ promiseCheckbox.addEventListener("change", function prosegui() {
         proceedBtn.className = "btn-disabled";
     }
 });
+
+proceedBtn.addEventListener("click", function () {
+    welcomePage.style.display = "none";
+    divQuestions.classList.remove("hidden");
+});
+
 
